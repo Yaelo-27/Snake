824f1fc3ed8b21db26ce1ebda64aab94f7623bb3
Yaelo-27 <78753436+Yaelo-27@users.noreply.github.com>
2021-11-15 23:44:48 +0100

    V15/11/23:11

--- app/src/main/java/com/proyectoYA/Snake/MainActivity.java
+++ app/src/main/java/com/proyectoYA/Snake/MainActivity.java
@@ -2,7 +2,9 @@ package com.proyectoYA.Snake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,4 +16,10 @@ public class MainActivity extends AppCompatActivity {
         //Con este comando quitamos la barra de estado de android, se debe cambiar en el manifest a noactionbar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
+
+    public void siguiente(View v){
+        Intent i = new Intent(this,Snake.class);
+        startActivity(i);
+    }
+
 }
